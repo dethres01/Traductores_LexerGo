5a3421226262e32b1cb3e04722c50cc3b9f44127
Hirane <diego.preciado@alumno.udg.mx>
2022-02-10 13:56:27 -0600

    Keywords, braces, parenthesis

--- lexer.go
+++ lexer.go
@@ -10,6 +10,7 @@ import (
 
 type Token int // Token types
 
+// Token types
 const (
 	EOF = iota
 	ILLEGAL
@@ -24,8 +25,37 @@ const (
 	DIV // /
 
 	ASSIGN // =
+	// Symbols
+	LPAREN // (
+	RPAREN // )
+	LBRACE // {
+	RBRACE // }
+	// Keywords.
+	KEYWORD
 )
 
+//ARRAY OF KEYWORDS
+var keywords = map[string]Token{
+	"int":      KEYWORD,
+	"return":   KEYWORD,
+	"if":       KEYWORD,
+	"else":     KEYWORD,
+	"while":    KEYWORD,
+	"for":      KEYWORD,
+	"break":    KEYWORD,
+	"continue": KEYWORD,
+	"bool":     KEYWORD,
+	"true":     KEYWORD,
+	"false":    KEYWORD,
+	"string":   KEYWORD,
+	"void":     KEYWORD,
+	"main":     KEYWORD,
+	"print":    KEYWORD,
+	"println":  KEYWORD,
+	"scan":     KEYWORD,
+}
+
+// tokens
 var tokens = []string{
 	EOF:     "EOF",
 	ILLEGAL: "ILLEGAL",
@@ -37,6 +67,11 @@ var tokens = []string{
 	MUL:     "*",
 	DIV:     "/",
 	ASSIGN:  "=",
+	KEYWORD: "KEYWORD",
+	LPAREN:  "(",
+	RPAREN:  ")",
+	LBRACE:  "{",
+	RBRACE:  "}",
 }
 
 func (t Token) String() string {
@@ -68,8 +103,10 @@ func (l *Lexer) Lex() (Position, Token, string) {
 			}
 			panic(err)
 		}
+		// GET TOKEN
 		l.pos.col++
 		switch r {
+		// INFIX OPERATORS
 		case '\n':
 			l.resetPosition()
 		case ';':
@@ -84,19 +121,32 @@ func (l *Lexer) Lex() (Position, Token, string) {
 			return l.pos, DIV, "/"
 		case '=':
 			return l.pos, ASSIGN, "="
+		case '(':
+			return l.pos, LPAREN, "("
+		case ')':
+			return l.pos, RPAREN, ")"
+		case '{':
+			return l.pos, LBRACE, "{"
+		case '}':
+			return l.pos, RBRACE, "}"
 		default:
 			if unicode.IsSpace(r) {
+				// If it's space do nothing
 				continue
 			} else if unicode.IsDigit(r) {
+				// check if it's a digit
 				startPos := l.pos
 				l.backup()
 				lit := l.lexInt()
 				return startPos, INT, lit
 			} else if unicode.IsLetter(r) {
+				// variables(identifiers)
+				// we have to check if it's a keyword
+
 				startPos := l.pos
 				l.backup()
-				lit := l.lexIdent()
-				return startPos, IDENT, lit
+				tok, lit := l.lexIdent()
+				return startPos, tok, lit
 			} else {
 				return l.pos, ILLEGAL, string(r)
 			}
@@ -133,26 +183,36 @@ func (l *Lexer) lexInt() string {
 		}
 	}
 }
-func (l *Lexer) lexIdent() string {
+func (l *Lexer) lexIdent() (Token, string) {
 	var lit string
 	for {
 		r, _, err := l.reader.ReadRune()
 		if err != nil {
 			if err == io.EOF {
-				return lit
+				return EOF, lit
 			}
 		}
 		l.pos.col++
 		if unicode.IsLetter(r) {
 			lit += string(r)
 		} else {
+			fmt.Println(lit)
+			if tok, ok := keywords[lit]; ok {
+				return tok, lit
+			}
 			l.backup()
-			return lit
+			return IDENT, lit
 		}
 	}
 }
 
 func main() {
+	if val, ok := keywords["pene"]; ok {
+		fmt.Println(val)
+	} else {
+		fmt.Println("not found")
+	}
+
 	file, err := os.Open("test.txt")
 	if err != nil {
 		panic(err)
